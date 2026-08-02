d68eca07ab61603217f2d020747a3b73332a90bc
Moi <alexis.rats2009@gmail.com>
2026-08-02 13:48:31 +0200

    Make TestChar a CharacterBody2D root instead of a Node2D wrapper
    Simplifies movement by driving velocity and move_and_slide on self.

--- Scenes/Characters/test_char.gd
+++ Scenes/Characters/test_char.gd
@@ -1,23 +1,23 @@
-extends Node2D
+extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @onready var player = $CharBody2D
 
 func _physics_process(delta):
-	if not player.is_on_floor():
-		player.velocity += player.get_gravity() * delta
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and player.is_on_floor():
-		player.velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		player.velocity.x = direction * SPEED
+		velocity.x = direction * SPEED
 	else:
-		player.velocity.x = move_toward(player.velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	player.move_and_slide()
+	move_and_slide()
